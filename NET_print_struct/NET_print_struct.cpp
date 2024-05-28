afd433f6ad9202e1003d4c6be318b448ad977039
Neto test <yury120405@gmail.com>
2024-05-28 16:28:27 +0400

    Initial commit

---
+++ NET_print_struct/NET_print_struct.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct adress {
+	string city;
+	string street;
+	int number_house;
+	int number_apartament;
+	int index;
+};
+
+void print(adress &person) {
+	cout << "Город: " << person.city << endl;
+	cout << "Улица: " << person.street << endl;
+	cout << "Номер дома: " << person.number_house << endl;
+	cout << "Номер квартиры: " << person.number_apartament << endl;
+	cout << "Индекс: " << person.index << endl;
+	cout << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	adress person1 = { "Москва", "Арбат", 12, 8, 123456};
+	adress person2 = { "Ижевск", "Пушкина", 59, 123, 953769 };
+	print(person1);
+	
+	print(person2);
+}
